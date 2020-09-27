c597327999eb63040f59173e2f58661f046b330c
YujiSue <yuji.sue@gmail.com>
2020-09-27 15:53:26 +0900

    update

--- src/3rd/crypto/md5c.c
+++ src/3rd/crypto/md5c.c
@@ -23,8 +23,7 @@ These notices must be retained in any copies of any part of this
 documentation and/or software.
  */
 
-#include "md5/global.h"
-#include "md5/md5.h"
+#include "crypto/md5.h"
 
 /* Constants for MD5Transform routine.
  */
